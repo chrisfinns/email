5312e9c8036e89b7f8e21e3c18d132979fa68f8c
chrisfinns <cf4856@gmail.com>
2023-07-21 21:51:48 +0900

    First commit

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,95 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  
+  
+  document.querySelector('#compose-form').onsubmit = send_email;
+
+
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+
+
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+
+  // Get Field Values of Compose view and Send Email
+
+
+
+  // Make a post request
+
+}
+
+
+
+function send_email() {
+    
+  const recipient = document.querySelector('#compose-recipients').value
+  const subject = document.querySelector('#compose-subject').value
+  const body = document.querySelector('#compose-body').value
+
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipient,
+      subject: subject,
+      body: body
+    })
+  })
+.then(response => response.json())
+.then(result => {
+    console.log(result)
+})
+  load_mailbox('sent');
+}
+
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  if ( mailbox === 'inbox') {
+    
+    fetch('/emails/inbox')
+    .then(response => response.json())
+    .then(emails => {
+      //console.log(emails);
+      console.log(emails.length)
+
+      for (let email in emails) {
+        let div = document.createElement('div');
+        div.innerHTML = email.sender
+        document.getElementById('emails-view').appendChild(div);
+
+      }
+
+    });
+
+  }
+
+
+}
